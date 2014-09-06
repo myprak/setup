baebb3e5cdb660c0fd8ff6e37af846bead8145bd
myprak <myprak@gmail.com>
2014-09-06 19:25:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,11 +19,10 @@ sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
 # Install crawler
 sudo apt-get install -y python-software-properties python g++ make
-sudo apt-get install -y python-dev libxml2-dev libxslt1-dev
+sudo apt-get install -y python-dev libxml2-dev libxslt1-dev python-lxml python-twisted
 sudo apt-get install -y --force-yes python-pip
+sudo pip install Scrapy Pillow w3lib cssselect
 
-
-#sudo apt-get install -y python-scrapy
 #sudo apt-get install -y python-dev libxml2-dev libxslt1-dev libxslt1.1 libxml2 libssl-dev
 
 #Install Postgres
