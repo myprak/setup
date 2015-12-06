86bdd8225e0019d3cdb5ea45b99b3f1f092456af
myprak <myprak@gmail.com>
2015-12-06 22:44:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@
 
 #yum update
 
+sudo yum install git-core
 
 cd $HOME
 export LANGUAGE=en_US.UTF-8
@@ -60,82 +61,6 @@ sudo pip install SQLAlchemy
 sudo apt-get install -y python-psycopg2
 
 
-# #-----Computer Vision Module-------
-
-
-# # OpencCV
-# cd $HOME
-# git clone https://github.com/Itseez/opencv.git -b 2.4
-# md opencv_bin
-# cd opencv_bin
-# cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_OPENCL=ON -DBUILD_NEW_PYTHON_SUPPORT=ON -DINSTALL_PYTHON_EXAMPLES=ON ~/opencv
-# echo "Installing Dependenices"
-# sudo apt-get -qq install -y libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg
-# make
-# sudo make install
-
-# sudo pip install numpy
-
-
-# # Install tesseract python wrapper
-# cd $HOME
-# wget http://python-tesseract.googlecode.com/files/python-tesseract_0.7-1.4_amd64.deb
-# sudo dpkg -i python-tesseract*.deb
-# sudo apt-get -f install
-
-
- 
-
-
-# -----Web server module------
-
-# install nodejs
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs
-
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
- 
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
-
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-sudo npm install -g jshint
-
-#Install npm express web development framework
-sudo npm install express
-
-#Install new node libraries
-sudo npm install cheerio
-sudo npm install commander
-sudo npm install restler
-sudo npm install request
-sudo npm install underscore
-
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
-sudo apt-get install -y rlwrap
-
-
-# Install Heroku toolbelt
-# https://toolbelt.heroku.com/debian
-sudo wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
-#Install Grunt and Twitter Bootstrap for RWD, plus Bower as package manager
-cd $HOME
-sudo npm install -g grunt-cli
-sudo npm install -g bower
-git clone http://github.com/twbs/bootstrap.git
-cd bootstrap/
-sudo npm install
-bower install --config.interactive=false bootstrap
-#first time running bower have to answer log reporting question case 
- 
 
 
 #-------Basic Module End--------
